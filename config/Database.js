9c92ed57013924b03583a78e9047287f740acbd2
slamerwin <34290462+slamerwin@users.noreply.github.com>
2025-10-26 15:59:02 +0700

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,8 @@
 import {Sequelize} from "sequelize";
 //skripsiportal_kaprodi
 //=1PZ[{-$Ug[9
-const db = new Sequelize('skripsi', 'root', '', {
-    'host': "localhost",
+const db = new Sequelize('railway', 'root', 'czoSXcgPHfXTgWKHwmApjHbFzXLrvBmy', {
+    'host': "mysql.railway.internal",
     'dialect': "mysql",
     'timezone': "+07:00",
 });
